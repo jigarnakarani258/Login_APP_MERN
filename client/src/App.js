0363165a81b449d36fe7aeab18abda6ec16ccc4d
Jigar Nakarani <97333469+jigarnakarani258@users.noreply.github.com>
2023-05-16 13:02:56 +0000

    Add tailwind css and configure react project dependencies.

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      ReactAPP
+
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      
     </div>
   );
 }
