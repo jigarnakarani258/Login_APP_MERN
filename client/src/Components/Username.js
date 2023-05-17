a13e737b1b60dd18c157126c5ab379b573e721b2
Jigar Nakarani <97333469+jigarnakarani258@users.noreply.github.com>
2023-05-17 13:28:10 +0000

    Add Toaster(alerts) and Formik for form handling

--- client/src/Components/Username.js
+++ client/src/Components/Username.js
@@ -2,24 +2,44 @@ import React from "react";
 import { Link } from "react-router-dom";
 import avatar from './../assets/profile.png'
 import styles from "./../styles/Username.module.css";
+import { Toaster } from "react-hot-toast";
+import { useFormik } from "formik";
+import  {usernameValidate}  from "./../helper/validate";
 
 function Username() {
 
+    const formik = useFormik({
+        initialValues : {
+            username : ''
+        },
+        validate : usernameValidate ,
+        validateOnBlur : false ,
+        validateOnChange : false ,
+        onSubmit : async values => {
+            console.log(values);
+        }
+
+    })
+
     return (
         <div className="container mx-auto">
+
+            <Toaster position="top-center" reverseOrder={false}></Toaster>
+
             <div className="flex justify-center items-center h-screen">
                 <div className={ styles.glass } >
 
                     <div className="title flex flex-col items-center">
-                        <h4 className="text-5xl font-bold"> 
-                             Hello Agian!! 
+                        <h4 className="text-5xl font-bold mt-2"> 
+                             Hello Avengers 
                         </h4>
                         <span className="py-4 text-xl w-2/3 text-center text-gray-500"> 
                              Explore more by connecting with us.. 
                         </span>
                     </div>
 
-                    <form className="py-1">
+                    <form className="py-1" onSubmit={formik.handleSubmit} >
+                        
                         <div className="profile flex justify-center py-4 "> 
                             <img 
                                 className={ styles.profile_img } 
@@ -27,11 +47,13 @@ function Username() {
                                 alt="avtar"
                             /> 
                         </div>
+
                         <div className="textbox flex flex-col items-center gap-6"> 
                             <input 
                                 className={ styles.textbox } 
                                 type="text"  
-                                placeholder="Username" 
+                                placeholder="Username"
+                                {...formik.getFieldProps('username')} 
                             />
                             <button 
                                 className={ styles.btn }  
@@ -41,8 +63,8 @@ function Username() {
 
                         <div className="text-center py-4">
                             <span> 
-                                Not a Member
-                                <Link className="text-red-500" href='/register'>
+                                Not a Member? 
+                                <Link className="text-red-500" to='/register'>
                                     Register Now
                                 </Link>
                             </span>
