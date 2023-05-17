7a38d81d7a3922a16640a2d46757ef53279e025f
Jigar Nakarani <97333469+jigarnakarani258@users.noreply.github.com>
2023-05-17 07:19:51 +0000

    Start username component Design

--- client/src/Components/Username.js
+++ client/src/Components/Username.js
@@ -1,10 +1,44 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Username() {
 
-    return(
-        <div>
-            Username Page 
+    return (
+        <div className="container mx-auto">
+            <div className="flex  h-screen">
+                <div>
+
+                    <div className="title flex flex-col items-center">
+                        <h4 className="text-5xl font-bold"> 
+                             Hello Agian!! 
+                        </h4>
+                        <span className="py-4 text-xl w-2/3 text-center text-gray-500"> 
+                             Explore more by connecting with us.. 
+                        </span>
+                    </div>
+
+                    <form className="py-1">
+                        <div className="profile flex justify-center py-4 "> 
+                            <img src=""  alt="avtar"/> 
+                        </div>
+                        <div className="textbox flex flex-col items-center gap-6"> 
+                            <input type="text"  placeholder="Username" />
+                            <button type='submit' > Submit </button>
+                        </div>
+
+                        <div className="text-center py-4">
+                            <span> 
+                                Not a Member
+                                <Link className="text-red-500" href='/register'>
+                                    Register Now
+                                </Link>
+                            </span>
+                        </div>
+
+                    </form>
+
+                </div>
+            </div>
         </div>
     )
 }
