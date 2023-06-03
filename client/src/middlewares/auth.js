20dec515a3d984bf098026f40069206af8e71eae
jigarnakarani258 <nakaranijigar258@gmail.com>
2023-06-03 16:23:43 +0530

    update AuthorizeUser for Profile component , protected route for password component

---
+++ client/src/middlewares/auth.js
@@ -0,0 +1,22 @@
+import { Navigate } from "react-router-dom";
+import { useAuthStore } from "../store/store";
+
+export const AuthorizeUser = ({ children }) => {
+  const token = localStorage.getItem("token");
+
+  if (!token) {
+    return <Navigate to={"/"} replace={true}></Navigate>;
+  }
+
+  return children;
+};
+
+export const ProtectRoutePassword = ({ children }) => {
+  const username = useAuthStore((state) => state.auth.username);
+
+  if (!username) {
+    return <Navigate to={"/"} replace={true}></Navigate>;
+  }
+
+  return children;
+};
